81483cde8495725f1b432fabb5acc9b5ae37a7cc
Winneyp <tatakornaow@gmail.com>
2024-11-01 00:52:44 +0700

    edit migration

--- database/migrations/05_create_user_address.php
+++ database/migrations/05_create_user_address.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->timestamps(); // Created at and updated at timestamps
         });
     }
-    
+
     /**
      * Reverse the migrations.
      */
